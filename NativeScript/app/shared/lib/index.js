a403de03e65fb056fe6c61909f7652b4cae63c52
LM. Garret <louis-maxence.garret@epfl.ch>
2018-11-06 13:55:15 +0100

    NativeScript: Restored lib folder

---
+++ NativeScript/app/shared/lib/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const dedjs = require("./dedjs");
+const file_io = require("./file-io/file-io");
+const files_path = require("./file-io/files-path");
+const scan_to_return = require("./scan-to-return/scan-to-return");
+
+module.exports = {
+    dedjs,
+    file_io,
+    files_path,
+    scan_to_return,
+};
